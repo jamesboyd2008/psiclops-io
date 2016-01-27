ae49e57375065f307b6ebdfd7b31317183603f93
wvrlock <nicholasrusso@icloud.com>
2016-01-26 20:01:23 -0800

    test

--- tests.py
+++ tests.py
@@ -10,15 +10,8 @@ class TestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    # def test(self):
-    #     pass
-
-    def test_home_status_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
-        result = self.app.get('/')
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
+    def test(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
