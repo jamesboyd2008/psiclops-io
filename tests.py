706c53368497e321ce15e2038efc3b980c2d3df2
wvrlock <nicholasrusso@icloud.com>
2016-01-26 19:44:29 -0800

    added tests.py

---
+++ tests.py
@@ -0,0 +1,17 @@
+from psiclops import app
+
+import os
+import unittest
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
